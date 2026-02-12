29c62f4bcf15dca5cd42d2f6c134d5f86b55af9c
Parleen Bagga <parleenkaurbagga@gmail.com>
2026-02-11 17:01:45 -0800

    Added code editor and review API integration

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes');
+const cors = require('cors');
+
 
 const app= express()
+app.use(cors());
 
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.send('Hellow World')
+    res.send('Hello World')
 })
 
 app.use('/ai',aiRoutes);
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,34 +1,63 @@
 import { useState, useEffect } from 'react'
 import "prismjs/themes/prism-tomorrow.css";
-
+import Editor from "react-simple-code-editor";
 import prism from "prismjs";
+import MarkDown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
+import "highlight.js/styles/github-dark.css";
+import axios from 'axios';
 import './App.css'
 // import { use } from '../../BackEnd/src/routes/ai.routes';
 
 function App() {
   const [count, setCount] = useState(0)
+  const [code, setCode] = useState(`function sum(){
+  return 1+1
+  }`)
+
+  const [review, setReview] = useState('');
+
   useEffect(()=>{
     prism.highlightAll();
-  })
+  },[])
+
+  async function reviewCode(){
+    const response = await axios.post('http://localhost:3000/ai/get-review',{code})
+    setReview(response.data);
+
+
+  }
 
   return (
     <>
     <main>
       <div className="left">
-        <div className="code"></div>
-        <pre>
-          <code className="language-javascript">
-            {`function sum(){
-            return 1+1
-            }`}
-
-          </code>
-        </pre>
-        <div className="review">Review</div>
-      </div>
-      <div className="right">
-        
-      </div>
+        <div className="code">
+          <Editor
+            value={code}
+            onValueChange={(code) => setCode(code)}
+            highlight={(code) => prism.highlight(code, prism.languages.js, 'javascript')}
+            padding={10}
+            style={{
+              fontFamily: '"Fira code", "Fira Mono", monospace',
+              fontSize: 16,
+              border:"1px solid #ddd",
+              borderRadius:"5px",
+              height:"100%",
+              width:"100%"
+            }}
+          />
+       
+        </div>
+        <div 
+        onClick ={reviewCode}
+        className="review">Review</div>
+    </div>
+    <div className="right">
+      <MarkDown
+        rehypePlugins={[rehypeHighlight]}
+      >{review}</MarkDown>
+    </div>
     </main>
     </>
   )
